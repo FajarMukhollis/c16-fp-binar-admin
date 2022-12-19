0d4b4374e2771f2d3dbcab0a72f906aac84dafbf
Fajar Mukhollis <fmukhollis@gmail.com>
2022-12-19 18:45:05 +0700

    change MainActivity XML, linear layout to appCompatButton

--- app/src/main/java/com/c16/flywithme_admin/presentation/home/MainActivity.kt
+++ app/src/main/java/com/c16/flywithme_admin/presentation/home/MainActivity.kt
@@ -2,11 +2,27 @@ package com.c16.flywithme_admin.presentation.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.c16.flywithme_admin.R
+import com.c16.flywithme_admin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var _binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+
+        _binding.btnDataCustomer.setOnClickListener {
+
+        }
+
+        _binding.btnDataFlight.setOnClickListener {
+
+        }
+
+        _binding.btnHistoryPayment.setOnClickListener {
+
+        }
     }
 }
